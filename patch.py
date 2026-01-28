644611ca0b6f4a13c9d4da4007d5bb338a6ec1df
sidhe <lambdacreature@gmail.com>
2026-01-28 14:26:02 -0500

    mejor nombre a algunas variables, uniforme entre scripts

--- patch.py
+++ patch.py
@@ -4,29 +4,29 @@ import sys
 CHUNK_SIZE = 512
 
 def main():
-    dest_path  = sys.argv[1]
-    start = eval(sys.argv[2])
-    patch_path = sys.argv[3]
-    out_path   = sys.argv[4]
+    target_path = sys.argv[1]
+    start  = eval(sys.argv[2])
+    patch_path  = sys.argv[3]
+    out_path    = sys.argv[4]
 
-    with open(dest_path, 'rb') as dest:
-        with open(patch_path, 'rb') as patch:
+    with open(target_path, 'rb') as target_file:
+        with open(patch_path, 'rb') as patch_file:
             with open(out_path, 'wb') as out_file:
                 remaining = start
                 while remaining > 0:
                     read_size = CHUNK_SIZE if remaining - CHUNK_SIZE >= 0 else remaining
-                    chunk = dest.read(read_size)
+                    chunk = target_file.read(read_size)
                     out_file.write(chunk)
 
                     remaining -= read_size
     
                 patch_size = 0
-                while (chunk := patch.read(CHUNK_SIZE)) != b'':
+                while (chunk := patch_file.read(CHUNK_SIZE)) != b'':
                     out_file.write(chunk)
                     patch_size += len(chunk)
 
-                dest.seek(start + patch_size)
-                while (chunk := dest.read(CHUNK_SIZE)) != b'':
+                target_file.seek(start + patch_size)
+                while (chunk := target_file.read(CHUNK_SIZE)) != b'':
                     out_file.write(chunk)
 
 
--- slice.py
+++ slice.py
@@ -4,19 +4,19 @@ import sys
 CHUNK_SIZE = 512   
 
 def main():
-    file_path = sys.argv[1]
-    start = eval(sys.argv[2])
-    end   = eval(sys.argv[3])
-    dest_path = sys.argv[4]
+    target_path = sys.argv[1]
+    start  = eval(sys.argv[2])
+    end    = eval(sys.argv[3])
+    out_path =    sys.argv[4]
 
     slice_size = end - start + 1
     remaining  = slice_size
-    with open(file_path, 'rb') as in_file:
-        with open(dest_path, 'wb') as out_file:
-            in_file.seek(start)
+    with open(target_path, 'rb') as target_file:
+        with open(out_path, 'wb') as out_file:
+            target_file.seek(start)
             while remaining > 0:
                 read_size = CHUNK_SIZE if remaining - CHUNK_SIZE >= 0 else remaining
-                chunk = in_file.read(read_size)
+                chunk = target_file.read(read_size)
                 out_file.write(chunk)
 
                 remaining -= CHUNK_SIZE
